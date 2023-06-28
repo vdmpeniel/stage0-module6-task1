eee78785c98c109a24aa8b1c53221e4a13efdb10
victor diaz <vdmpeniel@yahoo.es>
2023-06-28 12:39:54 -0400

    Added changes for all required subtasks.

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,26 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur){
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription(){
+        return  "This animal is mostly " + color +
+                ". It has " + numberOfPaws +
+                " paw" + (numberOfPaws > 1 ? "s" : "") + " and " +
+                (hasFur ? "a" : "no") +
+                " fur.";
+    }
+
+    public static void main(String[] args) {
+        Animal animal = new Animal("black", 5, false);
+        System.out.println(animal.getDescription());
+    }
 }
